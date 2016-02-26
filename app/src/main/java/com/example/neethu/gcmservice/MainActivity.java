1243f93013dc89061719fdf22aaf6e97af59bbee
neethu <neethump2014@gmail.com>
2016-02-26 17:36:15 +0530

    Gcmservice is added to git

---
+++ app/src/main/java/com/example/neethu/gcmservice/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.neethu.gcmservice;
+
+import android.content.BroadcastReceiver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  static  final int PLAY_SERVICES_RESOLUTION_REQUEST=9000;
+    private static final String TAG="mainactivity";
+    BroadcastReceiver mREgistrationBroadcastReceiver;
+    ProgressBar mRegistrationProgressBar;
+    TextView mInformationTextview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
